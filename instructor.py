5f7f58ab976ada2d8d1ecedfea86a30966afdc5a
John Long <jlcomp45@gmail.com>
2020-01-19 13:34:19 -0600

    completed inheritance chapter

--- instructor.py
+++ instructor.py
@@ -1,9 +1,8 @@
-class Instructor:
-    def __init__(self, first_name, last_name, slack, specialty, cohort):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.slack = slack
-        self.cohort = cohort
+from nss_person import NSSPerson
+
+class Instructor(NSSPerson):
+    def __init__(self, first_name, last_name, slack, cohort, specialty):
+        super().__init__(first_name, last_name, slack, cohort)
         self.specialty = specialty
 
     def assign_exercise(self, cohortobj, exerciseobj):
