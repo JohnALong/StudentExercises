44cb5a5c1afa3fbace6ba0ee29f5782219de33c9
John Long <jlcomp45@gmail.com>
2020-02-04 13:04:51 -0600

    completed student exercises chapter 4 and working on part 5

--- reports.py
+++ reports.py
@@ -172,10 +172,113 @@ class StudentExerciseReports():
             for exercise in select_exercises:
                 print(exercise)
 
+    def exercises_by_instructors(self):
+
+        """Retrieve all exercises and what instructor assigned them"""
+
+        with sqlite3.connect(self.db_path) as conn:
+
+            db_cursor = conn.cursor()
+
+            db_cursor.execute(""" SELECT
+                e.Exercise_Id Exercise_Id,
+                e.Exercise_Name,
+                i.Instructor_Id Instructor_Id,
+                i.First_Name,
+                i.Last_Name
+                FROM Exercises e 
+                JOIN Student_Exercise se ON se.Exercise_Id = e.Exercise_Id
+                JOIN Instructors i ON i.Instructor_Id = se.Instructor_Id;
+            """)
+
+            all_exercises_by_instructors = db_cursor.fetchall()
+
+            instructors = dict()
+
+            for exercise_instructor in all_exercises_by_instructors:
+                exercise_id = exercise_instructor[0]
+                exercise_name = exercise_instructor[1]
+                instructor_name = f'{exercise_instructor[3]} {exercise_instructor[4]}'
+
+                if instructor_name not in instructors:
+                    instructors[instructor_name] = [exercise_name]
+                else:
+                    instructors[instructor_name].append(exercise_name)
+
+            for instructor_name, exercises in instructors.items():
+                print(instructor_name)
+                for exercise in exercises:
+                    print(f'\t* {exercise}')
+
+    def exercises_with_students(self):
+        """Retrieve all exercises and the students working on each one"""
+
+        with sqlite3.connect(self.db_path) as conn:
+
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""select
+                e.Exercise_Id Exercise_Id,
+                e.Exercise_Name,
+                s.Student_Id Student_Id,
+                s.First_Name,
+                s.Last_Name
+                FROM Exercises e 
+                JOIN Student_Exercise se on se.Exercise_Id = e.Exercise_Id
+                JOIN Students s on s.Student_Id = se.Student_Id;
+             """)
+
+            all_exercises_with_students = db_cursor.fetchall()
+
+            # takes list of tuples and convert to dictionary to remove duplicates and create nested info for usability
+            exercises = dict()
+
+            for exercise_student in all_exercises_with_students:
+                exercise_id = exercise_student[0]
+                exercise_name = exercise_student[1]
+                student_id = exercise_student[2]
+                student_name = f'{exercise_student[3]} {exercise_student[4]}'
+
+                if exercise_name not in exercises:
+                    #1st square bracket is holding variable and denoting dictionary - 2nd bracket is denoting list
+                    # exercises[exercise_name] is adding a new key/value pair to the exercises dictionary, where exercise_name is the variable containing the key value which is the string
+                    exercises[exercise_name] = [student_name]
+                else:
+                    exercises[exercise_name].append(student_name)
+
+            # print students assigned to exercise without repeating exercise
+            for exercise_name, students in exercises.items():
+                print(exercise_name)
+                for student in students:
+                    print(f'\t* {student}')
+
+            students = dict()
+
+            for student_exercise in all_exercises_with_students:
+                exercise_id = student_exercise[0],
+                exercise_name = student_exercise[1],
+                student_id = student_exercise[2],
+                student_name = f'{student_exercise[3]} {student_exercise[4]}'
+
+                if student_name not in students:
+                    students[student_name] = [exercise_name]
+                else:
+                    students[student_name].append(exercise_name)
+
+            for student_name, exercises in students.items():
+                print(student_name)
+                for exercise in exercises:
+                    print(f'\t* {exercise[0]}')
+
+
+
 reports = StudentExerciseReports()
-reports.all_students()
-reports.all_instructors()
-reports.all_cohorts()
-reports.all_exercises()
-reports.js_exercises()
-reports.python_exercises()
+# reports.all_students()
+# reports.all_instructors()
+# reports.all_cohorts()
+# reports.all_exercises()
+# reports.js_exercises()
+# reports.python_exercises()
+# reports.exercises_with_students()
+reports.exercises_by_instructors()
+
